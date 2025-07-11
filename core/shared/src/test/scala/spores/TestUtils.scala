1134086de6723a45d9f4f5b60e81ea79de96d3f7
Jonas Spenger <jonas.spenger@gmail.com>
2025-07-11 12:47:03 +0200

    PR comments 8/X

--- core/shared/src/test/scala/spores/TestUtils.scala
+++ core/shared/src/test/scala/spores/TestUtils.scala
@@ -1,13 +1,16 @@
 package spores
 
+import scala.compiletime.testing.{typeChecks, typeCheckErrors}
+
+
 object TestUtils {
 
   inline def typeCheckSuccess(inline str: String): Boolean =
-    scala.compiletime.testing.typeChecks(str)
+    typeChecks(str)
 
   inline def typeCheckFail(inline str: String): Boolean =
-    !scala.compiletime.testing.typeChecks(str)
+    !typeChecks(str)
 
   inline def typeCheckErrors(inline str: String): List[String] =
-    scala.compiletime.testing.typeCheckErrors(str).map(_.message)
+    typeCheckErrors(str).map(_.message)
 }
